9cd54695b0c2d2675065930ddef519ceca73845e
LalaYun <77472513+LalaYun@users.noreply.github.com>
2022-01-24 17:37:42 +0900

    220124 - beginning of the project

---
+++ src/components/children.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>
+            {{alertMessage}}
+        </p>
+        {{parentMessage}}
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+
+@Component
+export default class Children extends Vue {
+    @Prop() parentMessage? : string; //parentMessage의 속성을 optional로 설정 ?
+    alertMessage : string = '';
+
+    @Watch('parentMessage') //immediate = 속성의 현재값으로 parentMessage를 즉시 호출한다, deep = 불러오는 것이 객체일 경우, 객체 내부의 속성까지 감시한다
+    update(value: string, oldValue: string){
+        this.alertMessage = '메세지를 업로드했습니다.'
+    }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <children :parentMessage = "message"></children>
+    <button@click="changeMessage">메세지를 바꿔줄게요</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import children from '@/components/children.vue';
+
+@Component({
+  components: {
+    children,
+  },
+})
+export default class Home extends Vue {
+  message: string = 'hello world';
+
+  changeMessage(){
+    this.message = 'change';
+  }
+}
+
+</script>
